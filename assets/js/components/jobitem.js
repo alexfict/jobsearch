67a1dd77465f2be5990574d7cfb28e7333e9d70a
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2020-02-16 13:20:20 +0100

    Implement shuffle feature Implement search feature (reverse) Styling with bootstrap

--- assets/js/components/jobitem.js
+++ assets/js/components/jobitem.js
@@ -3,14 +3,18 @@ Vue.component("job-item", {
     job: Object
   },
   template: '\
-    <div>\
-      <p>\
-        <span>Vil du bli {{ job.title }}?</span>\
-        <button>Ja</button>\
-        <button @click="removeFromList(job.id)">Nei</button>\
-      </p>\
+    <div class="col mb-4">\
+      <div class="card">\
+        <img v-bind:src="job.logo" class="card-img-top" v-bind:alt="job.title">\
+          <div class="card-body">\
+            <span>Vil du bli...?</span>\
+            <h5 class="card-title">{{job.title}}</h5>\
+            <button class="btn btn-primary">Ja</button>\
+            <button @click="removeFromList(job.id)" class="btn btn-secondary">Nei</button>\
+          </div>\
+      </div>\
     </div>\
-    ',
+  ',
   methods: {
     removeFromList: function(jobId) {
       this.$emit("drop-job-item", jobId);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 var app = new Vue({
   el: "#app",
   data: {
-    jobs: null
+    jobs: null,
+    searchInput: ""
   },
   mounted () {
     axios
@@ -11,6 +12,23 @@ var app = new Vue({
   methods: {
     dropJobItem: function(eventArg) {
       this.jobs = this.jobs.filter(job => job.id != eventArg);
+    },
+    shuffleJobs: function () {
+      for(let i = this.jobs.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * i);
+
+        let temp = this.jobs[i];
+        Vue.set(this.jobs, i, this.jobs[randomIndex]);
+        Vue.set(this.jobs, randomIndex, temp);
+      }
+    }
+  },
+  computed: {
+    filteredJobs: function() {
+      return this.searchInput == "" ? this.jobs : this.jobs.filter(job => job.title.includes(this.reversedSearchInput));
+    },
+    reversedSearchInput: function() {
+      return this.searchInput.split('').reverse().join('');
     }
   }
 });
